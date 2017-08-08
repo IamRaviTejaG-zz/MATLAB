361a0aa7dc330b67106ab6a2ef0d3c788126f284
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2017-08-08 18:15:48 +0530

    Create 02 - Matrix Operations.m

---
+++ 02 - Matrix Operations.m
@@ -0,0 +1,15 @@
+% Matrix Operations
+% Please enter matrices of same dimensions
+
+clear;
+clc;
+a = input('Enter array a: \n');
+b = input('\nEnter array b: \n');
+c = a+b
+d = a+b'
+e = a-b
+f = a*b
+g = a.*b
+h = a-b'
+i = a'+b'
+j = (a'+b)'
